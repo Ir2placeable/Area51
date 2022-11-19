059de42d89fec2028f5f3779534a1840a1f33997
ir2placeable <zxcv123594@gmail.com>
2022-11-19 21:23:57 +0900

    new victim has been created

--- python/greedy/2864.py
+++ python/greedy/2864.py
@@ -6,8 +6,21 @@
 # 5 또는 6의 위치를 파악해서, 아예 두 수를 만드는 것임.
 a, b = input().split(" ")
 
-print(a, b)
-
+min_a, min_b, max_a, max_b = '', '', '', ''
 for i in a:
-    print(i)
+    if i == '5' or i == '6':
+        max_a += '6'
+        min_a += '5'
+    else:
+        min_a += i
+        max_a += i
 
+for i in b:
+    if i == '5' or i == '6':
+        max_b += '6'
+        min_b += '5'
+    else:
+        min_b += i
+        max_b += i
+
+print(int(min_a) + int(min_b), int(max_a) + int(max_b))
