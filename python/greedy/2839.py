778c684d5a62b650e8cd43b8f53e9b76d6a353db
ir2placeable <zxcv123594@gmail.com>
2022-12-04 21:42:19 +0900

    12/4

--- python/greedy/2839.py
+++ python/greedy/2839.py
@@ -4,31 +4,14 @@
 # 경우의 수를 전부 구해보는 것이다..!
 
 sugar = int(input())
-bag3 = 0
-bag5 = 0
+bags = []
 
-temp = 0
-i3 = 0
-i5 = 0
-flag = False
-oppo = []
-while i5 <= sugar//5:
-    # print(i5)
-    i3 = 0
-    temp = 5 * i5 + 3 * i3
-    while temp <= sugar:
-        # print('i3 : ', i3)
-        temp = 5 * i5 + 3 * i3
-        if temp == sugar:
-            flag = True
-            break
-        i3 += 1
-    if flag:
-        oppo.append(i5 + i3)
-        flag = False
-    i5 += 1
+for i5 in range(0, sugar//5 + 1):
+    for i3 in range(0, sugar//3 + 1):
+        if i3*3 + i5*5 == sugar:
+            bags.append(i3+i5)
 
-if len(oppo) == 0:
+if len(bags) == 0:
     print(-1)
 else:
-    print(oppo[-1])
+    print(bags[-1])
