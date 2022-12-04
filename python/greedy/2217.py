ec6d6852a33abddba3db8a1f3f2a8b78229a2d7f
ir2placeable <zxcv123594@gmail.com>
2022-12-04 22:22:08 +0900

    12/4

--- python/greedy/2217.py
+++ python/greedy/2217.py
@@ -1,3 +1,16 @@
 # 2217 : 로프
 # https://www.acmicpc.net/problem/2217
 
+N = int(input())
+
+ropes = []
+for i in range(0, N):
+    ropes.append(int(input()))
+
+ropes.sort(reverse=True)
+max_weight = 0
+for i in range(0, N):
+    temp = ropes[i] * (i+1)
+    max_weight = max(max_weight, temp)
+
+print(max_weight)
