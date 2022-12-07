207363feda992a82a1218d7d97fad142f98e4fbb
ir2placeable <zxcv123594@gmail.com>
2022-12-07 20:54:23 +0900

    12/7

--- python/greedy/2847.py
+++ python/greedy/2847.py
@@ -1,3 +1,15 @@
 # 2847 : 게임을 만든 동준이
 # https://www.acmicpc.net/problem/2847
 
+levels = int(input())
+
+score_list = []
+for i in range(0, levels):
+    score_list.append(int(input()))
+
+dec = 0
+for i in range(levels-2, -1, -1):
+    if score_list[i] >= score_list[i+1]:
+        dec += score_list[i] - score_list[i+1] + 1
+        score_list[i] = score_list[i+1] - 1
+print(dec)
