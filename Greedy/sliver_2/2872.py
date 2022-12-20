d9beb60aea7faa54b5d3e861472def5acfb07d9f
ir2placeable <zxcv123594@gmail.com>
2022-12-20 11:07:54 +0900

    12/20

--- Greedy/sliver_2/2872.py
+++ Greedy/sliver_2/2872.py
@@ -1,15 +1,21 @@
 # 2872 : 우리집엔 도서관이 있어
 # https://www.acmicpc.net/problem/2872
 
+# 최대 수인 n 부터 하나씩 줄여가며 위로 올려야 한다.
+#
+
 n = int(input())
 books = []
 for i in range(0, n):
     books.append(int(input()))
 
-continuous = 0
-target = 0
+now = 0
+moves = 0
 for i in range(0, n):
-    if target < books[i]:
-        continuous += 1
-        target = books[i]
-print(n - continuous)
+    if books[i] == n:
+        moves += n-i-1
+        break
+    if books[i]+1 != books[i+1]:
+        moves += 1
+
+print(moves)
