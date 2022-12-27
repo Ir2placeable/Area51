c8ad1113496d6446d0f66b26b4c216263aa4552b
ir2placeable <zxcv123594@gmail.com>
2022-12-27 19:44:59 +0900

    add comment

--- Greedy/sliver_1/1052.py
+++ Greedy/sliver_1/1052.py
@@ -1,6 +1,9 @@
 # 1052 : 물병
 # https://www.acmicpc.net/problem/1052
 
+# n의 값을 1,2,4,8 형태의 조합으로 만든 후에, 그 개수가 k개를 넘지 않도록 한다.
+# ex) 13 2 -> [8, 4, 1] 조합으로 만든다. -> k=2 이므로 해당 리스트의 길이가 2가 되어야 한다.
+# 리스트를 2개 팝하고 그 차이를 bottles 로 기록한다.
 import sys
 
 n, k = map(lambda x: int(x), sys.stdin.readline().rstrip().split(" "))
