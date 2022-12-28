df4cdecc5a9e2968931079670ebdebe16ee86317
ir2placeable <zxcv123594@gmail.com>
2022-12-28 19:29:45 +0900

    12/28

--- Greedy/sliver_1/17615.py
+++ Greedy/sliver_1/17615.py
@@ -1,41 +1,19 @@
 # 17615 : 볼 모으기
 # https://www.acmicpc.net/problem/17615
 
-# 어떤 색의 공을 선택할지 기준을 찾아야 함 -> 핵심!
-# 맨 마지막 색의 공을 기준으로 선택해보자 -> 실패!
-# 모아져있는 공이 최대인 색의 반대 색을 선택해보자 -> 실패!
-# 공 개수가 작은 것을 선택해보자 -> 실패!
-# 연속된 공을 하나로 줄여보자
-
 import sys
 
 n = int(sys.stdin.readline())
-string = sys.stdin.readline().rstrip()
+balls = sys.stdin.readline().rstrip()
+moves = 10000000000
 
-balls = []
-for i in string:
-    balls.append(i)
+temp = balls.rstrip('R')
+moves = min(moves, temp.count('R'))
+temp = balls.rstrip('B')
+moves = min(moves, temp.count('B'))
+temp = balls.lstrip('R')
+moves = min(moves, temp.count('R'))
+temp = balls.lstrip('B')
+moves = min(moves, temp.count('B'))
 
-new_balls = []
-i = 0
-while i < n:
-    target = balls[i]
-    new_balls.append(target)
-    for j in range(i, n):
-        if balls[i] == target:
-            i += 1
-        else:
-            break
-
-# print(balls)
-# print(new_balls)
-
-new_balls.pop()
-a, b = 0, 0
-for ball in new_balls:
-    if ball == 'R':
-        a += 1
-    else:
-        b += 1
-
-print(min(a, b))
+print(moves)
