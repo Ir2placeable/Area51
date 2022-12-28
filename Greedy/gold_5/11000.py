8bbb00fa9bfc677e8e6889afeede21604f1f90f1
ir2placeable <zxcv123594@gmail.com>
2022-12-28 23:09:56 +0900

    fix

--- Greedy/gold_5/11000.py
+++ Greedy/gold_5/11000.py
@@ -11,8 +11,10 @@ for i in range(n):
 
 schedules.sort()
 # rooms : priority queue
+# sort() 함수는 O(N logN) 임 -> 여기서 시간 초과 발생. -> heapq를 사용해야 한다!
+# heappush(), heappop() 함수는 O(log N)임
+# 앞으로 우선순위큐는 heapq를 사용하자!
 rooms = []
-
 for schedule in schedules:
     if len(rooms) == 0:
         heapq.heappush(rooms, schedule[1])
