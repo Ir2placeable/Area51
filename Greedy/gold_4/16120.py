7aaaede863cc79199387279911465fd4cef019bb
ir2placeable <zxcv123594@gmail.com>
2023-01-09 17:42:43 +0900

    1/9

--- Greedy/gold_4/16120.py
+++ Greedy/gold_4/16120.py
@@ -1,20 +1,20 @@
 # 16120 : PPAP
 # https://www.acmicpc.net/problem/16120
 import sys
-from collections import deque
 
 string = sys.stdin.readline().rstrip()
 
 stack = []
 ace = 0
 for char in string:
-    if char == 'P' and ace == 0:
-        stack.append(char)
-    elif char == 'A':
-        ace = 1
-        stack.append(char)
-    elif char == 'P' and ace == 1:
-        ace = 0
-        for _ in range(3):
-            stack.pop()
-        stack.append(char)
+    if len(stack) > 3:
+        if stack[-4:] == ['P', 'P', 'A', 'P']:
+            for _ in range(4):
+                stack.pop()
+            stack.append('P')
+    stack.append(char)
+
+if stack == ['P'] or stack == ['P', 'P', 'A', 'P']:
+    print('PPAP')
+else:
+    print('NP')
