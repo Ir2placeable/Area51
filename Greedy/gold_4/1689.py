b910b0eb4b5c38e6f50d91a2be44f9b297e9871c
ir2placeable <zxcv123594@gmail.com>
2023-01-12 14:28:16 +0900

    fix typo

--- Greedy/gold_4/1689.py
+++ Greedy/gold_4/1689.py
@@ -1,8 +1,6 @@
 # 1689 : 겹치는 선분
 # https://www.acmicpc.net/problem/1689
 
-# 강의실 문제와 비슷한 유형이라고 생각됨
-
 import sys, heapq
 n = int(sys.stdin.readline())
 
@@ -12,9 +10,10 @@ for _ in range(n):
 lines.sort()
 # print(lines)
 
-# x[0]으로 정렬했기 때문에, x[0]은 항상 뒤에 것이 큼
+# x[0]으로 정렬했기 때문에, x[0]은 항상 앞에 것이 작다.
+# 그래서 heap 에는 x[1]만 넣어주면 된다.
 heap = [lines[0][1]]
-count = 0
+count = 1
 for i in range(1, n):
     line = lines[i]
     while heap and heap[0] <= line[0]:
