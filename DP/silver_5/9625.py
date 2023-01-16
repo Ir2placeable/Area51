d949cd385aebcf0b515a841b7d095c192b4b7b45
ir2placeable <zxcv123594@gmail.com>
2023-01-16 16:09:05 +0900

    1/16

--- DP/silver_5/9625.py
+++ DP/silver_5/9625.py
@@ -2,3 +2,14 @@
 # https://www.acmicpc.net/problem/9625
 
 import sys
+k = int(sys.stdin.readline())
+
+result = [[1, 0], [0, 1]]
+
+def dp(num):
+    if num < 2:
+        return result[num]
+
+    return [dp(num-1)[1], dp(num-1)[0] + dp(num-1)[1]]
+
+print(*dp(k))
