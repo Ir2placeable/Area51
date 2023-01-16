bfcb0ede394932f028753d60fef5dc728d9d590f
ir2placeable <zxcv123594@gmail.com>
2023-01-16 16:19:45 +0900

    1/16

--- DP/silver_5/13301.py
+++ DP/silver_5/13301.py
@@ -1,3 +1,18 @@
 # 13301 : 타일 장식물
 # https://www.acmicpc.net/problem/13301
 
+k = int(input())
+
+a = 4
+b = 6
+
+if k == 1:
+    print(a)
+elif k == 2:
+    print(b)
+elif k > 2:
+    while k > 2:
+        a, b = b, a+b
+        k -=1
+
+    print(b)
