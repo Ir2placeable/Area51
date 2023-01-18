974672926aedbcf87beeba467aa43eb537bd3cd8
ir2placeable <zxcv123594@gmail.com>
2023-01-18 19:56:40 +0900

    1/18

---
+++ DP/silver_4/15489.py
@@ -0,0 +1,3 @@
+# 15489 : 파스칼 삼각형
+# https://www.acmicpc.net/problem/15489
+
--- DP/silver_4/15624.py
+++ DP/silver_4/15624.py
@@ -2,16 +2,15 @@
 # https://www.acmicpc.net/problem/15624
 
 n = int(input())
+a, b = 0, 1
 
-a = 0
-b = 1
-
-if n == 1:
+if n == 0:
     print(0)
+if n == 1:
+    print(1)
 elif n == 2:
     print(1)
 else:
     for _ in range(n):
-        a, b = b % 1000000007, (a + b) % 1000000007
-
+        a, b = b, (a + b) % 1000000007
     print(a)
