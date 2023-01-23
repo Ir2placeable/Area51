5d31c730199e1a14d044984a2d519825f78616e3
ir2placeable <zxcv123594@gmail.com>
2023-01-23 13:02:35 +0900

    failed

--- SampleTest/G4_1987.py
+++ SampleTest/G4_1987.py
@@ -2,13 +2,11 @@
 
 # BFS 알고리즘을 베이스로 한 문제이다.
 # 통과조건에 이때까지 지나가지 않은 알파벳을 기준으로 한다. -> 알파벳은 26개로 고정되어 있으므로 미리 선언해서 사용한다.
-# 26개를 일일히 적기는 귀찮으므로 defaultdict 를 사용한다.
+# 26개를 일일히 적기는 귀찮으므로 ASCII를 사용한 배열을 만든다.
 
-# BFS인 줄 알았으나 DFS이다. -> 최대한 갈 수 있는 길 문제임
-# 갔다가 조건에 맞지 않으면 돌아와야 하므로 -> 백트래킹이 추가된다.
-# BFS + backtracking
 
 import sys
+from collections import deque
 
 height, width = map(lambda x: int(x), sys.stdin.readline().split())
 maps = []
@@ -20,28 +18,7 @@ for _ in range(height):
     maps.append(temp2)
 print(maps)
 
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
-
-def dfs(y, x, visited):
-    global result
-    result = max(result, len(visited))
-    print(x, y, visited)
-    for i in range(4):
-        ny = y + dy[i]
-        nx = x + dx[i]
-
-        if ny < 0 or nx < 0 or ny > height-1 or nx > width-1:
-            continue
-
-        if maps[ny][nx] in visited:
-            continue
-
-        visited.append(maps[ny][nx])
-        dfs(ny, nx, visited[:])
-
-
-result = 0
-temp = [maps[0][0]]
-dfs(0, 0, temp)
-print(result)
+visited = [0 for i in range(26)]
+visited[ord(maps[0][0])] = 1
+queue = deque([0, 0, visited])
+print(visited)
