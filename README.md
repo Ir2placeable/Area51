8b9b44c87179ca7a63fda7085c4594d1e20fabb9
ir2placeable <zxcv123594@gmail.com>
2023-01-28 17:18:50 +0900

    needs to be solved later

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ BFS, DFS, Dijkstra, Window Silding, Two pointer, DP
 stack, queue, deque, priority queue, set
 
 need review:
-2579 9095 1068 2212 21758 1092 2457 1987 20055 20437 15989 17651
+2579 9095 1068 2212 21758 1092 2457 1987 20055 20437 15989 17651 2110
--- SampleTest/G4_2110.py
+++ SampleTest/G4_2110.py
@@ -7,9 +7,22 @@ locations = []
 for _ in range(n):
     locations.append(int(sys.stdin.readline()))
 locations.sort()
-print(locations)
 
-diff = []
-for i in range(n-1):
-    diff.append(locations[i+1] - locations[i])
-print(diff)
+start, end = 1, locations[-1] - locations[0]
+result = 0
+while start <= end:
+    mid = (start + end) // 2
+    current = locations[0]
+    count = 1
+
+    for i in range(1, n):
+        if locations[i] >= current + mid:
+            count += 1
+            current = locations[i]
+
+    if count >= c:
+        start = mid + 1
+        result = mid
+    else:
+        end = mid - 1
+print(result)
