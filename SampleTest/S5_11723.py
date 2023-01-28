bea3a325229f5b7e4171814600d8a0c4f484ba8f
ir2placeable <zxcv123594@gmail.com>
2023-01-28 11:03:29 +0900

    1/28

--- SampleTest/S5_11723.py
+++ SampleTest/S5_11723.py
@@ -1,27 +1,31 @@
 # https://www.acmicpc.net/problem/11723
 
 # 간단한 set 다루기 문제로 보인다.
+# 메모리 제한이 있기 때문에, list 를 사용할 수 없다.
 import sys
 
-numbers = set()
+numbers = set([])
 n = int(sys.stdin.readline())
 for _ in range(n):
     temp = list(sys.stdin.readline().split())
-    if temp[0] == 'all':
-        numbers = set([i for i in range(1, 21)])
-    elif temp[0] == 'empty':
-        numbers = set()
-    elif temp[0] == 'add':
-        numbers.add(temp[1])
-    elif temp[0] == 'remove':
-        numbers.discard(temp[1])
-    elif temp[0] == 'check':
-        if temp[1] in numbers:
-            print(1)
-        else:
-            print(0)
-    elif temp[0] == 'toggle':
-        if temp[1] in numbers:
-            numbers.discard(temp[1])
-        else:
+    if len(temp) == 1:
+        if temp[0] == 'all':
+            numbers = set([str(i) for i in range(1, 21)])
+        elif temp[0] == 'empty':
+            numbers.clear()
+    else:
+        if temp[0] == 'add':
             numbers.add(temp[1])
+        elif temp[0] == 'remove':
+            numbers.discard(temp[1])
+        elif temp[0] == 'check':
+            if temp[1] in numbers:
+                print(1)
+            elif temp[1] not in numbers:
+                print(0)
+        elif temp[0] == 'toggle':
+            numbers.discard(temp[1])
+            if temp[1] in numbers:
+                numbers.discard(temp[1])
+            elif temp[1] not in numbers:
+                numbers.add(temp[1])
