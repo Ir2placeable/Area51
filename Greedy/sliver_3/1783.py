3880b930ac3dc3fda83a9b326befeb13f987a531
ir2placeable <zxcv123594@gmail.com>
2023-01-31 09:30:05 +0900

    1/31

--- Greedy/sliver_3/1783.py
+++ Greedy/sliver_3/1783.py
@@ -1,17 +1,31 @@
 # 1783 : 병든 나이트
 # https://www.acmicpc.net/problem/1783
 import sys
+from collections import deque
 height, width = map(lambda x: int(x), sys.stdin.readline().split(" "))
 
-point = []
-def init(a, b):
-    if 1 <= a <= height and 1 <= b <= width:
-        point.append([a, b])
-        init(a+1, b+2)
-        init(a+2, b+1)
-        init(a-1, b+2)
-        init(a-2, b+1)
+history = set()
+history.add((0, 0))
+queue = deque([(0, 0)])
 
-init(1, 1)
-print(point)
-print(len(point))
+dx = [1, 2, 2, 1]
+dy = [2, 1, -1, -2]
+
+while queue:
+    qx, qy = queue.popleft()
+
+    for i in range(4):
+        nx = qx + dx[i]
+        ny = qy + dy[i]
+
+        if nx < 0 or ny < 0 or nx > width-1 or ny > height-1:
+            continue
+
+        if (nx, ny) in history:
+            continue
+
+        print((nx, ny))
+        history.add((nx, ny))
+        queue.append((nx, ny))
+
+print(len(history))
