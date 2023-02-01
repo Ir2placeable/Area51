6d48e633fb9f60a7ca3d9fdb66f6b7a443e581f2
Ato <zxcv123594@gmail.com>
2023-02-01 11:24:44 +0900

    2/1

--- Implementation/silver_4/2331.py
+++ Implementation/silver_4/2331.py
@@ -9,11 +9,13 @@ d = defaultdict(int)
 d[n] = 1
 prev_number = n
 
-while n > 0:
+while True:
     next_number = 0
     for num in str(prev_number):
         next_number += int(num) ** k
 
+    if d[next_number] == 3:
+        break
     d[next_number] += 1
     prev_number = next_number
     n -= 1
