96346777de5c12045afbd3ee33d73152c8fdf328
Ato <zxcv123594@gmail.com>
2023-02-01 12:11:39 +0900

    comments removed

--- Implementation/silver_4/10157.py
+++ Implementation/silver_4/10157.py
@@ -1,32 +1,11 @@
 # https://www.acmicpc.net/problem/10157
 
-# c, r = 7, 6
-# k = x, y
-
-# 1 : 1, 1
-# 2 : 1, 2
-# 3 : 1, 3
-# 4 : 1, 4
-# 5 : 1, 5
-# 6 : 1, 6
-
-# 7 : 2, 6
-# 8 : 3, 6
-# 9 : 4, 6
-# 10 : 5, 6
-# 11 : 6, 6
-# 12 : 7, 6
-
-# 13 : 7, 5
-# 14 : 7, 4
-
 import sys
 x, y = map(lambda x: int(x), sys.stdin.readline().split())
 k = int(sys.stdin.readline())
 
 min_x, max_x = 1, x
 min_y, max_y = 1, y
-# 1,y / x,y / x,1 / 2,1 / 2,y-1 / x-1,y-1 /
 
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
