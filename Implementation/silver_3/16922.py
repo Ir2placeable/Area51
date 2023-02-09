adb14b6b88be8d5c3d207140852bad1426a023dc
Ato <zxcv123594@gmail.com>
2023-02-09 12:20:19 +0900

    refresh files

--- Implementation/silver_3/16922.py
+++ Implementation/silver_3/16922.py
@@ -1,12 +1,12 @@
 # https://www.acmicpc.net/problem/16922
 
-from itertools import permutations
 import sys
+from itertools import combinations_with_replacement
 
 n = int(sys.stdin.readline())
-base = [1, 5, 10, 50] * n
+numbers = [1, 5, 10, 50]
 
 result = set()
-for temp in permutations(base, n):
+for temp in combinations_with_replacement(numbers, n):
     result.add(sum(temp))
 print(len(result))
