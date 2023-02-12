c233707101c0832d0b488bcfe6bcc6feac3daebb
Ato <zxcv123594@gmail.com>
2023-02-12 23:24:44 +0900

    2/12

--- Samsung/D4_3752.py
+++ Samsung/D4_3752.py
@@ -1,18 +1,24 @@
 # 조합 문제
 # N : 최대 100
 
-# 1차 시도 : 제한시간 초과
-# combinations 를 구하는 과정에서 시간 초과가 발생했다.
-
-from itertools import combinations
-
 t = int(input())
 for test_case in range(1, t+1):
     n = int(input())
-    scores = set(map(lambda x: int(x), input().split()))
+    numbers = list(map(lambda x: int(x), input().split()))
 
-    cases = set()
-    combination_list = [0 for _ in range(n)] + list(scores)
-    for temp in combinations(combination_list, len(scores)):
-        cases.add(sum(temp))
-    print(len(cases))
+    length = sum(numbers) + 1
+    scores = [0 for _ in range(length+1)]
+    scores[0] = 1
+
+    while numbers:
+        score = numbers.pop()
+        next_scores = scores[:]
+
+        for i in range(length-1, -1, -1):
+            if scores[i] == 1 and i + score < length:
+                next_scores[i + score] = 1
+
+        scores = next_scores
+
+    result = sum(scores)
+    print("#%d %d" % (test_case, result))
