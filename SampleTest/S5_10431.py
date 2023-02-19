804d45857746fe2291a750adb10ffd7f9bfff281
Ato <zxcv123594@gmail.com>
2023-02-19 19:28:28 +0900

    2/19

--- SampleTest/S5_10431.py
+++ SampleTest/S5_10431.py
@@ -1,2 +1,28 @@
 # https://www.acmicpc.net/problem/10431
+import sys
+from collections import deque
 
+n = int(sys.stdin.readline())
+for _ in range(n):
+    temp = list(map(lambda x: int(x), sys.stdin.readline().split()))
+
+    case_num = temp[0]
+    nums = deque(temp[1:])
+    del temp
+
+    result = 0
+    line = deque([])
+    while nums:
+        num = nums.popleft()
+
+        if not line:
+            line.append(num)
+            continue
+
+        if line[0] > num:
+            result += len(line)
+            line.appendleft(num)
+        else:
+            line.append(num)
+
+    print("%d %d" % (case_num, result))
