71d92b7902373da3f25c0a3dac330cc3b6300c4b
Ato <zxcv123594@gmail.com>
2023-02-22 17:03:16 +0900

    2/22

--- SampleTest/G5_2668.py
+++ SampleTest/G5_2668.py
@@ -12,23 +12,27 @@ for i in range(n):
     visited = [0 for _ in range(n)]
     visited[i] = 1
 
-    flag = False
-    nex = arr[i][1]
+    # circular 조건 체크
+    isCircular = False
+    picked_number = arr[i][1]
     while True:
-        if nex == start:
-            flag = True
+        # 뽑은 숫자들이 circular 한 경우 -> 문제 조건에 부합한다.
+        if picked_number == start:
+            isCircular = True
             break
 
-        if visited[nex] == 1:
+        # 뽑은 숫자가 이미 뽑혀 있는 경우 -> 문제 조건에 부합하지 않는다.
+        if visited[picked_number] == 1:
             break
 
-        visited[nex] = 1
-        nex = arr[nex][1]
+        # 뽑은 숫자를 기록하고, 다음 숫자를 뽑는다.
+        visited[picked_number] = 1
+        nex = arr[picked_number][1]
 
-    # using visited
-    if not flag:
+    if not isCircular:
         continue
 
+    # 뽑은 숫자들이 circular 한 경우 -> 뽑은 숫자들을 기록한다.
     for j in range(n):
         if visited[j] == 1:
             result[j] = 1
