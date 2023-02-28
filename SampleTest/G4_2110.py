4b975e13577a1e5c7b94468e35b6b3dae77400f5
Ato <zxcv123594@gmail.com>
2023-02-28 15:47:56 +0900

    2/28

--- SampleTest/G4_2110.py
+++ SampleTest/G4_2110.py
@@ -1,28 +1,27 @@
 # https://www.acmicpc.net/problem/2110
-
 import sys
 
-n, c = map(lambda x: int(x), sys.stdin.readline().split())
-locations = []
-for _ in range(n):
-    locations.append(int(sys.stdin.readline()))
-locations.sort()
+n, target_APs = map(lambda x: int(x), sys.stdin.readline().split())
+nums = [int(sys.stdin.readline()) for _ in range(n)]
+nums.sort()
 
-start, end = 1, locations[-1] - locations[0]
+min_diff = 1
+max_diff = nums[-1] - nums[0]
 result = 0
-while start <= end:
-    mid = (start + end) // 2
-    current = locations[0]
-    count = 1
+
+while min_diff <= max_diff:
+    mid_diff = (min_diff + max_diff) // 2
+    current_AP = nums[0]
+    installed_APs = 1
 
     for i in range(1, n):
-        if locations[i] >= current + mid:
-            count += 1
-            current = locations[i]
+        if current_AP + mid_diff <= nums[i]:
+            installed_APs += 1
+            current_AP = nums[i]
 
-    if count >= c:
-        start = mid + 1
-        result = mid
+    if installed_APs >= target_APs:
+        min_diff = mid_diff + 1
+        result = mid_diff
     else:
-        end = mid - 1
+        max_diff = mid_diff - 1
 print(result)
