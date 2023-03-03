fd9d4c50473a987aa815c0739d66c2a49f951894
Ato <zxcv123594@gmail.com>
2023-03-03 16:44:39 +0900

    3/3

--- SampleTest/G5_13549.py
+++ SampleTest/G5_13549.py
@@ -2,41 +2,35 @@
 
 import heapq, sys
 
-x, target = map(lambda x: int(x), sys.stdin.readline().split())
+start_location, target_location = map(lambda x: int(x), sys.stdin.readline().split())
 total_cost = [sys.maxsize for i in range(100001)]
-total_cost[x] = 0
+total_cost[start_location] = 0
 
-queue = [[0, x]]
+queue = [[0, start_location]]
 result = 0
 
 while queue:
     current_cost, current_location = heapq.heappop(queue)
-    if current_location != 0 and target % current_location == 0:
+    if current_location == target_location:
         result = current_cost
         break
 
-    # 일반 이동 체크
     next_cases = []
-    # 다음 위치가 x-1 인 경우
-    if current_location > 0:
-        next_cases.append([current_cost + 1, current_location - 1])
     # 다음 위치가 x+1 인 경우
     if current_location < 100000:
         next_cases.append([current_cost + 1, current_location + 1])
+    # 다음 위치가 x-1 인 경우
+    if current_location > 0:
+        next_cases.append([current_cost + 1, current_location - 1])
+    # 다음 위치가 2*x 인 경우
+    if 0 < current_location <= 50000:
+        next_cases.append([current_cost, current_location * 2])
 
     for next_cost, next_location in next_cases:
         # 이미 과거의 왔던 비용보다, 현재 든 비용이 큰 경우
         if total_cost[next_location] < next_cost:
             continue
+
         total_cost[next_location] = next_cost
         heapq.heappush(queue, [next_cost, next_location])
-
-    # 순간 이동 체크
-    if 0 < current_location <= 50000:
-        for next_location in range(current_location, 100001, current_location):
-            if total_cost[next_location] < current_cost:
-                continue
-            total_cost[next_location] = current_cost
-        heapq.heappush(queue, [current_cost, current_location * 2])
-
 print(result)
