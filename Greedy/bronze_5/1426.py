0aaba14fdaa2c92aee74130ca2c9dfcfb440377b
Ato <zxcv123594@gmail.com>
2023-07-11 14:11:00 +0900

    fix : directories

--- Greedy/bronze_5/1426.py
+++
@@ -1,3 +0,0 @@
-# 1426 : 온라인 판매
-# https://www.acmicpc.net/problem/1246
-
--- SampleTest/G5_9655.py
+++
@@ -1,9 +0,0 @@
-# https://www.acmicpc.net/problem/9655
-import sys
-
-n = int(sys.stdin.readline())
-
-if n % 2 == 1:
-    print('SK')
-else:
-    print('CY')
